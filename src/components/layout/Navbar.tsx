93925ce86b2fdc2c7cd39e70da780fc396e568ef
Rizqi Setiawan <seiawanrizqi@gmail.com>
2026-06-29 05:50:43 +0700

    feat: change size Navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -26,8 +26,8 @@ const menus = [
 export default function Navbar() {
   return (
     <header className="border-b border-border">
-      <div className="mx-auto flex max-w-[1280px] items-center justify-between px-6 py-5">
-        <h1 className="text-2xl font-bold">Portofolio</h1>
+      <div className="mx-auto flex max-w-[1280px] items-center justify-between px-5 py-4">
+        <h1 className="text-4xl font-bold">Portofolio</h1>
 
         <nav>
           <ul className="flex gap-8">
